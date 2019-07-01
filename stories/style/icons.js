f8098b85280c57b59a7fc63b4d9a4d4c929d06bc
gabewomble <gwomble27@gmail.com>
2019-07-01 12:05:13 -0400

    feature(wlw-5): updated react style starter to wlw

---
+++ stories/style/icons.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { withReadme } from 'storybook-readme';
+import Case from 'case';
+import marked from 'marked';
+import * as icons from '../../src/images/icons';
+import style from './icons.scss';
+import { darkBackground } from '../constants';
+
+const renderImages = (images, containerSize, lightBg = false) => (
+  <div className={style['images-container']}>
+    {
+      Object.keys(images).map((key, index) => (
+        <div className={style['images-inner-container']}>
+          <div
+            className={`${style.image} ${lightBg ? style.light : ''}`}
+            key={index}
+            style={containerSize}
+          >
+            <img
+              alt={key}
+              src={images[key]}
+            />
+          </div>
+          <h4>
+            {Case.capital(key)}
+          </h4>
+        </div>
+      ))
+    }
+  </div>
+);
+
+storiesOf('Style', module)
+  .addDecorator(withReadme(marked(`
+# Icons (Images)
+
+You may use these icons like so:
+
+\`\`\`javascript
+import { images } from '@codeparticle/whitelabelwallet.styleguide';
+
+const Component2 = () => {
+  return <img src={images.icons.close} />;
+};
+\`\`\`
+`)))
+  .add('Icons', () => (
+    <div className={style.container}>
+      <h3>UI Icons</h3>
+      {renderImages(icons, { height: '200px', width: '200px' }, true)}
+    </div>
+  ), {
+    backgrounds: [{ ...darkBackground, default: false }],
+  });
