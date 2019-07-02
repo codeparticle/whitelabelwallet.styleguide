fc83c5ab05e2561140121cc27ed9eaabb8f94f0a
gabewomble <gwomble27@gmail.com>
2019-07-02 16:40:51 -0400

    feature(wlw-6): adding search component

---
+++ src/images/icons/index.js
@@ -0,0 +1,12 @@
+// SVG Files
+import close from './close.svg';
+import magnifyingGlass from './magnifying-glass.svg';
+
+// Component Files
+import MagnifyingGlassIcon from './magnifying-glass';
+
+export {
+  close,
+  magnifyingGlass,
+  MagnifyingGlassIcon,
+};
