e59257f296c371340581214e871fb1c2839d5e4f
Marc Mathieu <mmathieu.ctr@tenable.com>
2019-07-08 10:31:39 -0400

    feature(wlw-13): setting up toggle switch part 2

---
+++ src/components/toggle-switch/toggle-switch.js
@@ -0,0 +1,36 @@
+/**
++ * @fileoverview Toggle Switch Component
++ * @author Marc Mathieu
++ */
+// import React, { useState } from 'react';
+// import PropTypes from 'prop-types';
+// import classNames from 'classnames';
+// import uuidv1 from 'uuid/v1';
+// import styles from './toggle-switch.scss';
+
+// const ToggleSwitch = ({
+//   className,
+//   offLabel,
+//   onLabel,
+//   isDarkMode,
+//   children,
+//   disabled,
+//   fullWidth,
+//   ...rest
+// }) => (
+//   <div>
+//     <label className="switch">
+//       <input type="checkbox" />
+//       <span className="slider round" />
+//     </label>
+//   </div>
+// );
+
+// ToggleSwitch.propTypes = {
+//   className: PropTypes.string,
+//   offLabel: PropTypes.string,
+//   onLabel: PropTypes.string,
+//   isDarkMode: PropTypes.bool,
+// };
+
+// export default ToggleSwitch;
