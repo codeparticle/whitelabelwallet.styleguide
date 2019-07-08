20b2db745b5ef81d20b0d8600a8e524da139d905
Marc Mathieu <mmathieu.ctr@tenable.com>
2019-07-08 10:31:39 -0400

    feature(wlw-13): setting up toggle switch part 2

--- src/components/toggle-switch/toggle-switch.js
+++ src/components/toggle-switch/toggle-switch.js
@@ -2,7 +2,7 @@
 + * @fileoverview Toggle Switch Component
 + * @author Marc Mathieu
 + */
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import uuidv1 from 'uuid/v1';
@@ -12,47 +12,40 @@ import styles from './toggle-switch.scss';
 const ToggleSwitch = ({
   className,
   onClick,
+  value,
   ...rest
 }) => {
-  const [inputId] = useState(`toggle-switch-${uuidv1()}`);
+  const inputId = `toggle-switch-${uuidv1()}`;
   const theme = useTheme('toggle');
 
-
   return (
-    <div
-      className={classNames(
-        styles['toggle-switch'],
-        'toggle-switch'
-      )}
-    >
+    <div className={classNames(styles['toggle-switch'])}>
       <label
         className={classNames(
-          styles['toggle-switch-label'],
-          'toggle-switch-label'
+          styles['toggle-switch-label']
         )}
         htmlFor={inputId}
       >
-        <input {...rest} onClick={onClick} type="checkbox" className={classNames(styles['toggle-switch-checkbox'])} id={inputId} />
-        <span className={classNames(
-          styles['toggle-switch-slider'],
-          'toggle-switch-slider')}
+        <input
+          {...rest}
+          onClick={onClick}
+          type="checkbox"
+          className={classNames(styles['toggle-switch-checkbox'])}
+          id={inputId}
+          value={value}
         />
-        <span className={classNames(
-          styles['toggle-switch-btn'],
-          'toggle-switch-btn'
-        )}
-        />
+        <span className={classNames(styles['toggle-switch-slider'])} />
+        <span className={classNames(styles['toggle-switch-btn'])} />
         <style jsx>
           {`
-              .toggle-switch-slider {
-                &:before {
-                  background-color: ${theme.onBackground};
-                }
-                &.toggle-switch-slider:after {
-                  background-color: ${theme.offBackground};
-                }
-              }
-            `}
+            .${styles['toggle-switch-slider']} {
+              background-color: ${value ? theme.onBackground : theme.offBackground};
+              color: ${theme.fontColor};
+            }
+            .${styles['toggle-switch-btn']} {
+              background-color: ${theme.button};
+            }
+          `}
         </style>
       </label>
     </div>
@@ -62,11 +55,13 @@ const ToggleSwitch = ({
 ToggleSwitch.propTypes = {
   className: PropTypes.string,
   onClick: PropTypes.func,
+  value: PropTypes.bool,
 };
 
 ToggleSwitch.defaultProps = {
   className: '',
   onClick: null,
+  value: false,
 };
 
 export { ToggleSwitch };
