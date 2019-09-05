04156f97cc6c4a3765d9ccab2007aa23585e8e46
gabewomble <gwomble27@gmail.com>
2019-09-05 14:10:58 -0400

    feature(wlw-83): added theme-provider, adjusted sidepanel theme

--- src/components/theme-provider/theme-context.js
+++ src/components/theme-provider/theme-context.js
@@ -3,6 +3,36 @@
  * @author Gabriel Womble
  */
 import React from 'react';
+import PropTypes from 'prop-types';
 import { themes } from './themes';
+import { THEME_KEYS } from './theme-keys';
 
-export const ThemeContext = React.createContext(themes.light);
+const { DARK, LIGHT } = THEME_KEYS;
+
+const ThemeContext = React.createContext(themes.light);
+
+function ThemeProvider({
+  themeKey,
+  children,
+}) {
+  const theme = themes[themeKey];
+
+  return (
+    <ThemeContext.Provider value={theme}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
+
+ThemeProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+  themeKey: PropTypes.oneOf([
+    DARK,
+    LIGHT,
+  ]).isRequired,
+};
+
+export {
+  ThemeContext,
+  ThemeProvider,
+};
