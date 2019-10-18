7a44f6f6af9c2af8ee9b2a4be7e9be2d75e5747e
Marc Mathieu <mmathieu.ctr@tenable.com>
2019-10-18 13:03:46 -0400

    feature(wlw-168): add event data onRowClick callback

--- src/components/list/list-item.js
+++ src/components/list/list-item.js
@@ -119,7 +119,7 @@ export function ListItem({
     childIcon: icon,
   };
 
-  function handleSelection() {
+  function handleSelection(event) {
     if (allowDeselect && isSelected) {
       setSelected({});
 
@@ -135,6 +135,7 @@ export function ListItem({
       data,
     };
 
+    selectedValue.data = { ...selectedValue.data, event };
     setSelected(selectedValue);
     onRowClicked(selectedValue.data);
   }
